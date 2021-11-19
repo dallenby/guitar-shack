eb04ab886e09a2f3d387e6dbd937ed790e876a45
Daniel Allenby <dallenby@ford.com>
2021-11-19 10:52:10 +0000

    Passing test

--- src/test/java/com/guitarshack/ReorderAlertTest.java
+++ src/test/java/com/guitarshack/ReorderAlertTest.java
@@ -2,12 +2,10 @@ package com.guitarshack;
 
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
-
 public class ReorderAlertTest {
 
     @Test
     public void test() {
-        fail();
+
     }
 }
