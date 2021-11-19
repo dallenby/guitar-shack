8f2a8465463e35d36fa4a63a7c3d5a32826d6102
Theo Brown <tbrow403@ford.com>
2021-11-19 12:32:04 +0000

    Added a test for when product needs reordering

--- src/main/java/com/guitarshack/Product.java
+++ src/main/java/com/guitarshack/Product.java
@@ -1,7 +1,16 @@
 package com.guitarshack;
 
 public class Product {
+    private final int id;
+    private final int stock;
+
     public Product(int id, int stock) {
 
+        this.id = id;
+        this.stock = stock;
+    }
+
+    public int getStock() {
+        return stock;
     }
 }
--- src/main/java/com/guitarshack/ReorderChecker.java
+++ src/main/java/com/guitarshack/ReorderChecker.java
@@ -1,11 +1,19 @@
 package com.guitarshack;
 
 public class ReorderChecker {
-    public ReorderChecker(Warehouse warehouse, ReorderThreshold reorderThreshold, ReorderAlert reorderAlert) {
+    private final Warehouse warehouse;
+    private final ReorderThreshold reorderThreshold;
+    private final ReorderAlert reorderAlert;
 
+    public ReorderChecker(Warehouse warehouse, ReorderThreshold reorderThreshold, ReorderAlert reorderAlert) {
+        this.warehouse = warehouse;
+        this.reorderThreshold = reorderThreshold;
+        this.reorderAlert = reorderAlert;
     }
 
     public void onSale(int productId, int quantity) {
-
+        Product product = warehouse.getProduct(productId);
+        Boolean reorder = (product.getStock() - quantity) < reorderThreshold.calculate(product);
+        if(reorder) reorderAlert.alert();
     }
 }
