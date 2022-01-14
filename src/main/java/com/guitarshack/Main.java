69901ab46e803d5b29be7bff142d353a40e4f27c
Daniel Allenby <allenbyd@outlook.com>
2022-01-14 12:57:19 +0000

    Implement basic web server
    Co-Authored-By: AristotleAxolotl <9041824+AristotleAxolotl@users.noreply.github.com>

--- src/main/java/com/guitarshack/Main.java
+++ src/main/java/com/guitarshack/Main.java
@@ -1,7 +1,23 @@
 package com.guitarshack;
 
-public class Main {
-    public static void main(String[] args) {
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpServer;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpContext context = server.createContext("/");
+        context.setHandler(exchange -> {
+            String s = "Hello World";
+            exchange.sendResponseHeaders(200, s.getBytes().length);
+            OutputStream response = exchange.getResponseBody();
+            response.write(s.getBytes());
+            response.close();
+        });
+        server.start();
     }
 }
