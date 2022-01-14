698ff8604cd916e4ca3b28c2fb28afde4a45ecfe
Daniel Allenby <allenbyd@outlook.com>
2022-01-14 17:20:39 +0000

    Implement alert in ReorderProduct

--- src/main/java/com/guitarshack/ReorderProduct.java
+++ src/main/java/com/guitarshack/ReorderProduct.java
@@ -3,6 +3,10 @@ package com.guitarshack;
 public class ReorderProduct implements Reorder {
     @Override
     public void alert(String message) {
-
+        String authorisation = System.getenv("AUTHORISATION");
+        String phoneNumber = System.getenv("PHONE_NUMBER");
+        WebApi<SmsResponse> api = new WebApi<SmsResponse>("https://api.thesmsworks.co.uk/v1/message/send", SmsResponse.class);
+        SmsMessageBuilder messageBuilder = new SmsMessageBuilder(message, phoneNumber, "test");
+        api.post(messageBuilder.generateBodyAsString(), authorisation);
     }
 }
